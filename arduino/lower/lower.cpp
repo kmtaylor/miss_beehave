f2770f316d34596b32689f962b14a3cbd5fb1103
Kim Taylor <kmtaylor@gmx.com>
2022-12-11 22:58:01 +1100

    Add empty arduino project.

---
+++ arduino/lower/lower.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+
+void setup() {
+    Serial.begin(115200);
+    Serial1.begin(115200);
+}
+
+void loop() {
+    if (Serial1.available()) {
+        Serial.write(Serial1.read());
+    }
+    if (Serial.available()) {
+        Serial1.write(Serial.read());
+    }
+}
+
