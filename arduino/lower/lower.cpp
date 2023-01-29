b7c88130f23ec633bb11d40c8460ffb0f408fae5
Kim Taylor <kmtaylor@gmx.com>
2023-01-29 17:36:17 +1100

    Skeleton arduino modbus implementation with forwarding to roboteq

--- arduino/lower/lower.cpp
+++ arduino/lower/lower.cpp
@@ -1,16 +1,40 @@
 #include <Arduino.h>
+#include <Modbusino.h>
 
-void setup() {
-    Serial.begin(115200);
-    Serial1.begin(115200);
+#define MODBUS_BAUD		    115200
+#define MODBUS_SLAVE		    3
+#define MODBUS_IDLE		    ((uint16_t) -1)
+
+enum modbus_regs_e {
+    LED_STATE = 0,
+    MB_REGS_SIZE,
+};
+
+static void modbus_forward(uint8_t *msg, uint8_t msg_length) {
+    Serial1.write(msg, msg_length);
 }
 
+static uint16_t mb_regs[MB_REGS_SIZE] = { MODBUS_IDLE };
+static ModbusinoSlave modbusino_slave(MODBUS_SLAVE, modbus_forward);
+
+void setup() {
+    modbusino_slave.setup(MODBUS_BAUD);
+    Serial1.begin(MODBUS_BAUD);
+}
+
+#define MB_ACTION(offset) if (((mb_val = mb_regs[offset]) != MODBUS_IDLE) \
+				&& (mb_regs[offset] = MODBUS_IDLE))
 void loop() {
-    if (Serial1.available()) {
+    uint16_t mb_val;
+
+    while (Serial1.available()) {
         Serial.write(Serial1.read());
     }
-    if (Serial.available()) {
-        Serial1.write(Serial.read());
+
+    if (modbusino_slave.loop(mb_regs, MB_REGS_SIZE) > 0) {
+	MB_ACTION(LED_STATE) {
+            digitalWrite(LED_BUILTIN, mb_val);
+        }
     }
 }
 
