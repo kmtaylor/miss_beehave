0c877f774ae8c2f74113155ddae43ec07e1fa86c
base <base@missbeehave.my.domain>
2023-04-07 22:39:41 +1000

    Tidy up ADC read, connect to motor command.

--- arduino/lower/irq_task.h
+++ arduino/lower/irq_task.h
@@ -1,6 +1,8 @@
 /* Run a function on timer 5 overflow interrupt.
  * Runs at 16MHz/64/512 ~= 488Hz */
 
+#define IRQ_TASK_RATE_HZ    488
+
 extern "C" {
     void irq_task_setup(void (*task)(void));
 }
--- arduino/lower/lower.cpp
+++ arduino/lower/lower.cpp
@@ -8,9 +8,31 @@
 #define MODBUS_SLAVE    3
 #define MODBUS_IDLE     ((uint16_t) -1)
 
+#define ADC_I2C_ADDR        0x48
+#define ADC_NUM_CHANNELS    2
+
+/* Read ADC over I2C when not equal to -1 */
+static volatile int8_t adc_ctx = -1;
+
+static void irq_task(void) {
+    static uint16_t i;
+    static uint8_t val;
+    static int8_t adc_channel;
+
+    adc_channel = (adc_channel + 1) % ADC_NUM_CHANNELS;
+    adc_ctx = adc_channel;
+
+    if (i-- == 0) {
+        digitalWrite(LED_BUILTIN, val);
+        val = !val;
+        i = IRQ_TASK_RATE_HZ;
+    }
+}
+
 enum modbus_regs_e {
     MB_LED_STATE = 0,
-    MB_ADC_VAL,
+    MB_ADC_VAL0,
+    MB_ADC_VAL1,
     MB_REGS_SIZE,
 };
 
@@ -21,15 +43,24 @@ static void modbus_forward(uint8_t *msg, uint8_t msg_length) {
 static uint16_t mb_regs[MB_REGS_SIZE] = { MODBUS_IDLE };
 static ModbusinoSlave modbusino_slave(MODBUS_SLAVE, modbus_forward);
 
-void irq_task(void) {
-    static uint16_t i;
-    static uint8_t val;
+static uint16_t read_adc(uint8_t channel) {
+    uint16_t val;
 
-    if (i-- == 0) {
-        //digitalWrite(LED_BUILTIN, val);
-        val = !val;
-        i = 488;
-    }
+    Wire.beginTransmission(ADC_I2C_ADDR);
+    Wire.write(0x00); /* Conversion register */
+    Wire.endTransmission();
+    Wire.requestFrom(ADC_I2C_ADDR, 2);
+    val  = Wire.read() << 4;
+    val += Wire.read() >> 4;
+
+    /* Start next conversion */
+    Wire.beginTransmission(ADC_I2C_ADDR);
+    Wire.write(0x01); /* Configuration register */
+    Wire.write(0xC5 | (channel << 4));
+    Wire.write(0x83); /* Defaults */
+    Wire.endTransmission();
+
+    return val;
 }
 
 void setup() {
@@ -42,33 +73,15 @@ void setup() {
 #define MB_ACTION(offset) if (((mb_val = mb_regs[offset]) != MODBUS_IDLE) \
                                 && (mb_regs[offset] = MODBUS_IDLE))
 
-static uint16_t read_adc(void) {
-    uint16_t val;
-
-    Wire.beginTransmission(0x48);
-    Wire.write(0x00);
-    Wire.endTransmission();
-    Wire.requestFrom(0x48, 2);
-    val  = Wire.read() << 4;
-    val += Wire.read() >> 4;
-
-    // Setup next read
-    Wire.beginTransmission(0x48);
-    Wire.write(0x01);
-    Wire.write(0xC5);
-    Wire.write(0x83); /* Defaults */
-    Wire.endTransmission();
-
-    return val;
-}
-
 void loop() {
     uint16_t mb_val;
-    static uint16_t i;
+    int8_t adc_channel;
 
-    if (i-- == 0) {
-        mb_regs[MB_ADC_VAL] = read_adc();
-        i = 10000;
+    adc_channel = adc_ctx;
+    if (adc_channel >= 0) {
+        mb_regs[adc_channel == 0 ? MB_ADC_VAL0 : MB_ADC_VAL1] = 
+            read_adc(adc_channel);
+        adc_ctx = -1;
     }
 
     while (Serial1.available()) {
