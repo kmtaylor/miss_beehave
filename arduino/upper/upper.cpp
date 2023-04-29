922fad5f60e2499f321707b7d3f68b4165cae103
Kim Taylor <kmtaylor@gmx.com>
2023-04-29 16:59:33 +1000

    Map RC channels to array in puredata.

--- arduino/upper/upper.cpp
+++ arduino/upper/upper.cpp
@@ -8,7 +8,6 @@
 
 enum modbus_regs_e {
     MB_LED_STATE = 0,
-    MB_CHAN_0,
     MB_CHAN_1,
     MB_CHAN_2,
     MB_CHAN_3,
@@ -16,8 +15,6 @@ enum modbus_regs_e {
     MB_CHAN_5,
     MB_CHAN_6,
     MB_CHAN_7,
-    MB_CHAN_8,
-    MB_CHAN_9,
     MB_REGS_SIZE,
 };
 
@@ -45,7 +42,6 @@ void loop() {
 
     rc_ibus.process();
     if (rc_ibus.available()) {
-        mb_regs[MB_CHAN_0] = rc_ibus.get(0);
         mb_regs[MB_CHAN_1] = rc_ibus.get(1);
         mb_regs[MB_CHAN_2] = rc_ibus.get(2);
         mb_regs[MB_CHAN_3] = rc_ibus.get(3);
@@ -53,8 +49,6 @@ void loop() {
         mb_regs[MB_CHAN_5] = rc_ibus.get(5);
         mb_regs[MB_CHAN_6] = rc_ibus.get(6);
         mb_regs[MB_CHAN_7] = rc_ibus.get(7);
-        mb_regs[MB_CHAN_8] = rc_ibus.get(8);
-        mb_regs[MB_CHAN_9] = rc_ibus.get(9);
     }
 }
 
