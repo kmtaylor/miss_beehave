748922a53f7a137fbe62b1bc758d6ed7151c75df
Kim Taylor <kmtaylor@gmx.com>
2023-07-02 19:43:17 +1000

    Add modbusino. Toggles direct connection.

--- arduino/stm32/hello_world.cpp
+++ arduino/stm32/hello_world.cpp
@@ -1,17 +1,34 @@
 #include <Arduino.h>
+#include <Modbusino.h>
 
-/*
-This sketch is just a simple "hello world", transposed to the world of Arduino.
-It uses Serial to print "hello world" every second.
-*/
+/* 10 Directly connected pins:
+ * PA15, PC10-12, PD0-4, PD7 */
 
-// the setup function runs once when you press reset or power the board
+#define MODBUS_BAUD     115200
+#define MODBUS_SLAVE    5
+#define MODBUS_IDLE     ((uint16_t) -1)
+
+enum modbus_regs_e {
+    MB_LED_STATE = 0,
+    MB_REGS_SIZE,
+};
+
+static uint16_t mb_regs[MB_REGS_SIZE] = { MODBUS_IDLE };
+static ModbusinoSlave modbusino_slave(MODBUS_SLAVE);
+
 void setup() {
-  Serial.begin(9600); // Initialize the Serial connection. 9600 is the standard baud rate value for Arduino.
+    pinMode(PA15, OUTPUT);
+    modbusino_slave.setup(MODBUS_BAUD);
 }
 
-// the loop function runs over and over again forever
+#define MB_ACTION(offset) if (((mb_val = mb_regs[offset]) != MODBUS_IDLE) \
+                                && (mb_regs[offset] = MODBUS_IDLE))
 void loop() {
-  Serial.println("hello world"); // print "hello world" to the Serial console
-  delay(1000); // wait 1000ms (1s)
+    uint16_t mb_val;
+
+    if (modbusino_slave.loop(mb_regs, MB_REGS_SIZE) > 0) {
+        MB_ACTION(MB_LED_STATE) {
+            digitalWrite(PA15, mb_val);
+        }
+    }
 }
