f70b19bc786416019e0bf7ef6b3c871f327b584f
Kim Taylor <kmtaylor@gmx.com>
2024-11-05 22:38:35 +1100

    Send AS5600 position over Modbus.

--- micropython/main.py
+++ micropython/main.py
@@ -1,19 +1,23 @@
-import machine, time
-import uos, pyb
+import machine, time, uos, as5600
 from umodbus.serial import ModbusRTU
 
+#Setup pins
+machine.Pin(machine.Pin.board.OUT1,  mode=machine.Pin.ALT, alt=5) # SCL 
+machine.Pin(machine.Pin.board.ESTOP, mode=machine.Pin.ALT, alt=6) # SDA out
+machine.Pin(machine.Pin.board.P1X,   mode=machine.Pin.ALT, alt=6) # SDA in
+machine.Pin(machine.Pin.board.P10X,  mode=machine.Pin.ALT, alt=6) # Required dummy sclk
+
 led = machine.Pin('CPX')
-for i in range(20):
-    led.value(not led.value())
-    time.sleep(0.1)
+
+def as5600_poll(t):
+    as5600.poll()
+
+as5600_timer = machine.Timer(-1, freq = 1000, callback = as5600_poll)
 
 uos.dupterm(None, 1)
 client = ModbusRTU(5)
 
-def toggle_led(reg_type, address, val):
-    led.value(not led.value())
-
-client.add_hreg(0xF000, on_set_cb = toggle_led)
-
 while True:
     result = client.process()
+    client.set_hreg(4, as5600.get_status())
+    client.set_hreg(5, as5600.get_pos())
