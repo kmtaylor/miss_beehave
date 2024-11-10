76249cb003a2eb1b2117d98a800265c17215be81
Kim Taylor <kmtaylor@gmx.com>
2024-11-10 16:53:42 +1100

    Add servo PID loop.

--- micropython/main.py
+++ micropython/main.py
@@ -1,23 +1,34 @@
-import pyb, uos
+import pyb, uos, time
 from umodbus.serial import ModbusRTU
 from servo import HeadServo
 
 pwm_timer = pyb.Timer(9, freq=1000)
 pwm_channel = pwm_timer.channel(2, pyb.Timer.PWM, pin=pyb.Pin('DIC'))
 dir_pin = pyb.Pin('CPC')
-servo = HeadServo(1000, pwm_channel, dir_pin)
+head_servo = HeadServo(1000, pwm_channel, dir_pin)
+
+def servo_print():
+    while True:
+        head_servo.print()
+        time.sleep(0.5)
+
+def servo_pos(reg_type, address, val):
+    head_servo.set_pos = val[0]
+    head_servo.enable = True
 
 mb = ModbusRTU(5)
-mb.set_hreg(1, 0) # PWM
-mb.set_hreg(2, 0) # Direction
+mb.add_hreg(1, on_set_cb=servo_pos) # Position
 
 def modbus_main():
     uos.dupterm(None, 1)
     while True:
-        result = mb.process()
-        servo.set_pwm(mb.get_hreg(1))
-        servo.set_dir(mb.get_hreg(2))
-        mb.set_hreg(4, servo.get_status())
-        mb.set_hreg(5, servo.get_pos())
+        mb.set_hreg(2, head_servo.cur_pos)
+        mb.set_hreg(3, round(head_servo.cor_p))
+        mb.set_hreg(4, round(head_servo.cor_i))
+        mb.set_hreg(5, round(head_servo.cor_pi))
+        mb.set_hreg(6, head_servo.get_status())
+        mb.process()
 
-#modbus_main()
+#servo_pos(None, None, [800])
+#servo_print()
+modbus_main()
--- micropython/servo.py
+++ micropython/servo.py
@@ -1,7 +1,16 @@
 import machine, as5600
 
 class HeadServo:
-    def __init__(self, freq, pwm_channel, dir_pin):
+    def __init__(self, freq, pwm_channel, dir_pin, pid_config = None):
+        if pid_config:
+            self.pid_max = pid_config[0]
+            self.pid_p = pid_config[1]
+            self.pid_i = pid_config[2]
+        else:
+            self.pid_max = 90
+            self.pid_p = 10
+            self.pid_i = 0.1
+
         # I2C pins: (SCL, SDA out, SDA in, required dummy sclk)
         machine.Pin('OUT1',  mode=machine.Pin.ALT, alt=5)
         machine.Pin('ESTOP', mode=machine.Pin.ALT, alt=6)
@@ -9,24 +18,40 @@ class HeadServo:
         machine.Pin('P10X',  mode=machine.Pin.ALT, alt=6)
 
         self.timer = machine.Timer(-1, freq=freq, callback=self.process)
-        self.count = 0
-        self.freq = freq
         self.pwm_channel = pwm_channel
         self.dir_pin = dir_pin
+        self.enable = False
+
+        self.set_pos = 0
+        self.cur_pos = 0
+        self.cor_p = 0
+        self.cor_i = 0
+        self.cor_pi = 0
 
     def process(self, timer):
-        if self.count == self.freq - 1:
-            #print(as5600.get_pos())
-            self.count = 0
+        self.cur_pos = as5600.get_pos()
+        err = self.set_pos - self.cur_pos
 
-        self.count += 1
+        self.cor_p = self.pid_p * err
+        self.cor_p = max(-self.pid_max, min(self.cor_p, self.pid_max))
+        self.cor_i += self.pid_i * err
+        self.cor_i = max(-self.pid_max - self.cor_p, 
+                         min(self.cor_i, self.pid_max - self.cor_p))
+        self.cor_pi = self.cor_p + self.cor_i
+
+        if self.enable:
+            self.set_dir(self.cor_pi < 0)
+            self.set_pwm(abs(self.cor_pi))
+
         as5600.poll()
+
+    def print(self):
+        print(f"Set: {self.set_pos}, Pos: {self.cur_pos}, " +
+              f"cor_p: {self.cor_p}, cor_i: {self.cor_i}, " +
+              f"cor_pi: {self.cor_pi}")
 
     def get_status(self):
         return as5600.get_status()
-
-    def get_pos(self):
-        return as5600.get_pos()
 
     def set_dir(self, dir):
         self.dir_pin.value(dir)
